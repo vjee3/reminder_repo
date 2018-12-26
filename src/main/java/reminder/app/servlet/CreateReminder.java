56e62117c671de9f073848648a7d940d8c8eb057
pankaj <pankaj@pankaj-Vostro-3478>
2018-12-27 01:35:02 +0530

    url path changed in Servlet ("/") required

--- src/main/java/reminder/app/servlet/CreateReminder.java
+++ src/main/java/reminder/app/servlet/CreateReminder.java
@@ -8,7 +8,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet("create")
+@WebServlet("/create")
 public class CreateReminder extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
--- src/main/java/reminder/app/servlet/ViewReminder.java
+++ src/main/java/reminder/app/servlet/ViewReminder.java
@@ -13,20 +13,25 @@ import javax.servlet.http.HttpServletResponse;
 
 import reminder.app.db.DatabaseConnection;
 
-@WebServlet("view")
+@WebServlet("/view")
 public class ViewReminder extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
 	
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		 try {
 			 Connection con=DatabaseConnection.getConnectivity();
 			 String query="select * from rem1";
 			 Statement stmt=con.createStatement();
 			 ResultSet rs=stmt.executeQuery(query);
+			 String str ="";
+			 
 			while(rs.next()) {
+				str =rs.getInt(1) + " " + rs.getString(2) + " " + rs.getString(3) + " " + rs.getString(4);
 				System.out.println(rs.getInt(1) + " " + rs.getString(2) + " " + rs.getString(3) + " " + rs.getString(4)); 
 			}
+			
+			response.sendRedirect("index.jsp?test="+str);
 		 }catch(Exception e) {
 			 
 		 }
